24956839d09e45a22301a060225098124f4c2af2
CircleLin <wayne7428@hotmail.com>
2020-10-02 11:03:28 +0800

    加入專案檔案。

---
+++ BlazorFileUpload/Server/Controllers/UploadController.cs
@@ -0,0 +1,41 @@
+﻿using BlazorFileUpload.Shared;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorFileUpload.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment env;
+
+        public UploadController(IWebHostEnvironment env)
+        {
+            this.env = env;
+        }
+
+        [HttpPost]
+        public  IActionResult PostAsync(UploadedFile uploadedFile)
+        {
+            try
+            {
+                var path = $"{env.WebRootPath}\\{uploadedFile.FileName}";               
+                var fs = System.IO.File.Create(path);
+                fs.Write(uploadedFile.FileContent, 0, uploadedFile.FileContent.Length);
+                fs.Close();
+
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest("Save File Failed");
+            }
+        }
+    }
+}
